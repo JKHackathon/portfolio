289bfd521d7812623e9a8dfb22a9de41c581508d
JKHackathon <penguinkim21@gmail.com>
2024-05-13 00:47:14 -0700

    Development case study outline

--- index.js
+++ index.js
@@ -18,26 +18,3 @@ function handleScroll() {
 }
 
 window.addEventListener("scroll", handleScroll);
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const sections = document.querySelectorAll('.section');
-
-//     function changeSectionStyle() {
-//         sections.forEach(section => {
-//             const rect = section.getBoundingClientRect();
-//             const isVisible = (rect.top >= 0 && rect.bottom <= window.innerHeight);
-
-//             if (isVisible) {
-//                 if (section.id === 'landing') {
-//                     document.body.style.backgroundColor = 'black';
-//                 } else if (section.id === 'about') {
-//                     document.body.style.backgroundColor = 'white';
-//                 } else if (section.id === 'projects') {
-//                     document.body.style.backgroundColor = 'black';
-//                 }
-//             }
-//         });
-//     }
-
-//     document.addEventListener('scroll', changeSectionStyle);
-// });
